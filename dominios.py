4c2e8ad53ecfbc412a9c6c9de69ac9f1ce3507f3
Carlos G. Gavidia <cgavidiac@gmail.com>
2021-06-09 14:30:19 +0100

    Solucion al tercer taller

--- dominios.py
+++ dominios.py
@@ -1,25 +1,49 @@
 def obtener_dominio(dominio_por_pais, pais):
+    if pais in dominio_por_pais.keys():
+        return dominio_por_pais[pais]
     return None
 
 
+def formatear_cadena(cadena, longitud):
+    return (cadena + " " * longitud)[:longitud]
+
+
 def mostrar_valores(diccionario):
-    print("")
+    longitud_columna = 10
+
+    if not diccionario.keys():
+        print("No hay valores que mostrar.")
+
+    for clave in diccionario.keys():
+        dominio = diccionario[clave]
+        print(formatear_cadena(clave + ":", longitud_columna) + formatear_cadena(dominio, longitud_columna))
 
 
 def registrar_dominio(dominio_por_pais, pais, dominio):
-    print("")
+    if obtener_dominio(dominio_por_pais, pais) is None:
+        dominio_por_pais[pais] = dominio
+    else:
+        print(f"El pais {pais} ya se encuentra registrado con el dominio {dominio_por_pais[pais]}.")
 
 
 def actualizar_dominio(dominio_por_pais, pais, nuevo_dominio):
-    print("")
+    if obtener_dominio(dominio_por_pais, pais) is not None:
+        dominio_por_pais[pais] = nuevo_dominio
+    else:
+        print(f"El pais {pais} no esta registrado.")
 
 
 def generar_pais_por_dominio(diccionario_dominios):
-    return {}
+    pais_por_dominio = {}
+    for pais in diccionario_dominios.keys():
+        dominio = diccionario_dominios[pais]
+        pais_por_dominio[dominio.upper()] = pais.upper()
+
+    return pais_por_dominio
 
 
 if __name__ == "__main__":
     dominios = {"Peru": "per",
                 "Brasil": "br",
                 "Chile": "cl"}
-    mostrar_valores(dominios)
+    print(generar_pais_por_dominio(dominios))
